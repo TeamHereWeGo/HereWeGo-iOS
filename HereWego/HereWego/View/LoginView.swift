22fe6d77dba457506adb399740c8d2b3f9b8f373
KyungsooLee <jmtkd9196@gmail.com>
2022-11-09 02:02:51 +0900

    [FEATURE/HWG-5] feat: SignUpView(회원가입) 구현 및 연결

--- HereWego/HereWego/View/LoginView.swift
+++ HereWego/HereWego/View/LoginView.swift
@@ -11,41 +11,50 @@ struct LoginView: View {
     @State var id: String = ""
     @State var pw: String = ""
     var body: some View {
-        VStack {
-            Spacer()
-            Image("UCL_logo")
-                .resizable()
-                .aspectRatio( contentMode: .fit)
-                .frame(width: 200, height: 200)
-            Spacer()
-            TextField("ID", text: $id)
-                .padding()
-                .background(Color.gray)
-                .cornerRadius(5.0)
-                .padding(.horizontal, 20)
-            TextField("PW", text: $pw)
-                .padding()
-                .background(Color.gray)
-                .cornerRadius(5.0)
-                .padding(.horizontal, 20)
-            Button {
-                
-            } label: {
-                Text("Login")
+        NavigationView {
+            
+            VStack {
+                Spacer()
+                Image("UCL_logo")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                Spacer()
+                TextField("ID", text: $id)
                     .padding()
                     .background(Color.gray)
                     .cornerRadius(5.0)
                     .padding(.horizontal, 20)
-            }
-            Spacer()
-            
-            Text("아이디/비밀번호 찾기")
-            Text("회원가입")
-            HStack {
-                Text("Oauth 연동")
-                Image(systemName: "checkmark.square")
+                TextField("PW", text: $pw)
+                    .padding()
+                    .background(Color.gray)
+                    .cornerRadius(5.0)
+                    .padding(.horizontal, 20)
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .padding()
+                        .background(Color.gray)
+                        .cornerRadius(5.0)
+                        .padding(.horizontal, 20)
+                }
+                Spacer()
                 
+                Text("아이디/비밀번호 찾기")
+                NavigationLink {
+                    SignUpView()
+                        .navigationBarTitleDisplayMode(.inline)
+                } label: {
+                    Text("회원가입")
+                }
+                HStack {
+                    Text("Oauth 연동")
+                    Image(systemName: "checkmark.square")
+                }
             }
+            
+            
         }
     }
 }
