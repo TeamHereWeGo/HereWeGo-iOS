2c5afbff2cdcfee968d3bc73e62f7f8af33db54f
KyungsooLee <jmtkd9196@gmail.com>
2022-11-09 15:35:42 +0900

    [test] test: test commit

--- HereWego/HereWego/View/TeamListRow.swift
+++ HereWego/HereWego/View/TeamListRow.swift
@@ -42,7 +42,6 @@ struct TeamListRow: View {
                             .foregroundColor(.gray)
                             .padding(.trailing)
                     }
-                    
                 }
             }
         }
