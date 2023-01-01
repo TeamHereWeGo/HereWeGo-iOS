b7125067ac5e68083cdca5b63a9c43425060befb
KyungsooLee <jmtkd9196@gmail.com>
2023-01-01 21:08:11 +0900

    [FEATURE/HWG-15] feat: Oauth 활용을 위한 LoginButton View 구현

---
+++ HereWego/HereWego/View/Login/LoginButton.swift
@@ -0,0 +1,75 @@
+//
+//  LoginButton.swift
+//  HereWeGo
+//
+//  Created by Kyungsoo Lee on 2022/12/23.
+//
+
+import SwiftUI
+
+struct LoginButton: View {
+    var provider : String
+    var body: some View {
+        VStack {
+            Image("UCL_logo")
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .frame(width: 200, height: 200)
+            Button {
+                
+            } label: {
+                HStack{
+                    Text("구글로 로그인")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(width: 300, height: 50)
+                        .background(Color.gray)
+                        .cornerRadius(5.0)
+                }
+            }
+            Button {
+                
+            } label: {
+                HStack{
+                    Text(provider + "로 로그인")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(width: 300, height: 50)
+                        .background(Color.green)
+                        .cornerRadius(5.0)
+                }
+            }
+            Button {
+                
+            } label: {
+                HStack{
+                    Text("깃허브로 로그인")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black)
+                        .cornerRadius(5.0)
+                }
+            }
+            Button {
+                
+            } label: {
+                HStack{
+                    Text("카카오로 로그인")
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .frame(width: 300, height: 50)
+                        .background(Color.yellow)
+                        .cornerRadius(5.0)
+                }
+            }
+   
+        }
+    }
+}
+
+struct LoginButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginButton(provider: "네이버")
+    }
+}
