1fc2cc92b5590ebeb7e3a9073cbf06f8ff199b8f
KyungsooLee <jmtkd9196@gmail.com>
2023-05-12 17:01:22 +0900

    [feature/HWG-21]feat: CharView 임시 데이터 차트 구현

---
+++ HereWego/HereWego/View/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  HereWeGo
+//
+//  Created by Kyungsoo Lee on 2023/05/12.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
